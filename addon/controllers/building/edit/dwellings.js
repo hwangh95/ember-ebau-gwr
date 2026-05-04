b8b926b38083c60f85d277fd54fe4fa20e8387e7
Dawn Hwang <dawn.hwang@adfinis.com>
2026-05-04 14:02:12 +0200

    feat(dwellings): refactor removal modal to reusable component

--- addon/controllers/building/edit/dwellings.js
+++ addon/controllers/building/edit/dwellings.js
@@ -3,11 +3,8 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import { task } from "ember-concurrency";
-import RemoveDwellingValidations from "ember-ebau-gwr/validations/remove-dwelling";
 
 export default class BuildingEditDwellingsController extends Controller {
-  RemoveDwellingValidations = RemoveDwellingValidations;
-
   @service building;
   @service dwelling;
   @service intl;
